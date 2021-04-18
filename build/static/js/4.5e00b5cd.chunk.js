ac7674eaa61cd04353b08baecea61d0e444e935d
Tom Knight <tomknight.dev@gmail.com>
2021-04-18 23:22:04 +0100

    Added build folder

---
+++ build/static/js/4.5e00b5cd.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpknight-net"]=this["webpackJsonpknight-net"]||[]).push([[4],{74:function(t,e,n){"use strict";n.r(e),e.default=n.p+"static/media/RustWebServer.c7cc45f7.png"}}]);
+//# sourceMappingURL=4.5e00b5cd.chunk.js.map
