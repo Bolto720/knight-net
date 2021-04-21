6cdd291aa856693da10db504eb7847c8fa5540c6
Tom Knight <tomknight.dev@gmail.com>
2021-04-21 22:00:30 +0100

    Added a brief description

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ function App() {
   return (
     <div className="App">
       <h1 className="Title">Welcome to Knight-Net</h1>
+      <h3 className="Description">Below are a few of the repos from my github account.
+          </h3>
       <Repo/>
     </div>
   );
