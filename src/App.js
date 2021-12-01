7e60c841e62cd20dbd48d6f24ee4e088fe06a1ab
Tom Knight <tom.knight@pushdoctor.io>
2021-12-01 16:37:13 +0000

    Removed container from title

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
 
   return (
     <div className="App">
-      <h1 className="Title">Welcome to Knight-Net in a Container</h1>
+      <h1 className="Title">Welcome to Knight-Net</h1>
       <h3 className="Description">Below are a few of the repos from my github account.</h3>
       <Repo/>
     </div>
